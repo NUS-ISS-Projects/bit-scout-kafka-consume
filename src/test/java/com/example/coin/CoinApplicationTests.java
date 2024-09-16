ddbdf07e1ad6b11badedd7b78299627bd97f8ada
ngtianxun08 <e1327862@u.nus.edu>
2024-09-16 14:47:34 +0800

    * Excluded kafka during tests.

--- src/test/java/com/example/coin/CoinApplicationTests.java
+++ src/test/java/com/example/coin/CoinApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.coin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.example.coin.service.KafkaProducerService;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = { KafkaAutoConfiguration.class })
 class CoinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private KafkaProducerService kafkaProducerService;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
